e8720725e0b55eb9a5b3ece3e7c99b0460f7a6f2
oluwafemi <makanjuolafemi17@gmail.com>
2022-10-12 11:11:05 +0100

    done

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="bg-gray-800 h-16 flex items-center justify-center text-white">
+            <p>Copyright &copy; 2022 Makay | All rights reserved.</p>
+        </div>
+    )
+}
+
+export default Footer
